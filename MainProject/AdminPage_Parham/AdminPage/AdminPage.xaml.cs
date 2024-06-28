2fec5dd33db6da6b8ce93579cb7784cec8867ef7
Parham Mohammadi <parham200483@gmail.com>
2024-06-28 03:58:05 +0330

    Admin-page is done

---
+++ MainProject/AdminPage_Parham/AdminPage/AdminPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainProject.AdminPage_Parham.AdminPage
+{
+    public partial class AdminPage : Window
+    {
+        public AdminPage()
+        {
+            InitializeComponent();
+        }
+        private void Restaurant_Registration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Search_ON_Restaurants_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Search_ON_Complaints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Last_Unread_Complaints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Answer_Last_Unread_Complaints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void All_Complaints_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
