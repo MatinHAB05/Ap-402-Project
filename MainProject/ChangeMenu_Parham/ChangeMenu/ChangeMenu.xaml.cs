cc659dee90c996506a70fa4ab0816bc3a24cf3f9
Matin Hasanali Baki <152606115+MatinHAB05@users.noreply.github.com>
2024-07-02 16:37:53 +0330

    merging[UI_parham to develop] (#1)
    * No Edits! rather than 701692492737 just structure of UI_Parham
    
    * Admin-Page is done
    
    * Admin-page is done
    
    * Profile Page Completed
    
    * Restaurant Panel Completed
    
    * Customer main page completed
    
    * Restaurant Change menu form completed
    
    * Restaurant change menu completed
    
    * Update MainProject.csproj
    
    * Final Update MainProject.csproj
    
    * Last Final Update MainProject.csproj
    
    ---------
    
    Co-authored-by: Parham Mohammadi <parham200483@gmail.com>
    Co-authored-by: parham200483 <62794248+parham200483@users.noreply.github.com>

---
+++ MainProject/ChangeMenu_Parham/ChangeMenu/ChangeMenu.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainProject.ChangeMenu_Parham.ChangeMenu
+{
+    public partial class ChangeMenu : Window
+    {
+        public ChangeMenu()
+        {
+            InitializeComponent();
+        }
+        private void Food_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Delete_Food_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Add_A_Food_Button(Object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MainProject/RestaurantPanel_Parham/RestaurantPanel/RestaurantPanel.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainProject.RestaurantPanel_Parham.RestaurantPanel
+{
+    public partial class RestaurantPanel : Window
+    {
+        public RestaurantPanel()
+        {
+            InitializeComponent();
+        }
+        private void Change_Menu(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Change_food_inventory(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Activate_the_reservation_service(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        private void Order_and_reservation_history(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
