5d93aad0fbbd9b80b482d15e40096c2e2c6a27b2
Matin Hasanali Baki <152606115+MatinHAB05@users.noreply.github.com>
2024-07-03 19:52:10 +0330

    Update and clean MainWindow.xaml.cs
    Update and clean MainWindow.xaml.cs

--- MainProject/MainWindow.xaml.cs
+++ MainProject/MainWindow.xaml.cs
@@ -24,10 +24,6 @@ namespace MainProject
             InitializeComponent();
             LoginPage loginPage = new LoginPage();
             loginPage.Show();
-            //LoginPage login = new LoginPage();
-            //login.Show();
-            //CustomerMainPage customerMainPage = new CustomerMainPage();
-            //customerMainPage.Show();
             this.Close();
         }
     }
