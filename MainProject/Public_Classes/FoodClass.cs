eae293d58ee017d3ce504bfb9d631fb84e6c027a
Matin Hasanali Baki <152606115+MatinHAB05@users.noreply.github.com>
2024-07-04 16:30:32 +0330

    Update FoodClass.cs

--- MainProject/Public_Classes/FoodClass.cs
+++ MainProject/Public_Classes/FoodClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +17,7 @@ namespace MainProject.Public_Classes
         public List<Comment> comments_IN_ORDER { get; set; }
         public string FoodCategory {  get; set; }
         public string Image_Path { get; set; }
+
         public FoodClass() { }
         public FoodClass(string FoodCategory,string Name ,int FoodID ,double price, List<string> Raw_Materials , double xBar , int  RemNumber , List<Comment> comments_IN_Order , string image_Path)
         {
@@ -25,8 +26,7 @@ namespace MainProject.Public_Classes
             this.FoodID = FoodID;
             this.price = price;
             this.Raw_Materials = Raw_Materials;
-            //this.xBar = xBar;
-            this.xBar = 0;
+            this.xBar = xBar;
             this.RemNumber = RemNumber;
             this.comments_IN_ORDER = comments_IN_Order;
             this.Image_Path = image_Path;
