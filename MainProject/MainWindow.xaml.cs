6ac3f5f7b41678c4ecb4212af173ea6b908bf4f3
Matin Hasanali Baki <m9652973@gmail.com>
2024-07-05 07:17:38 +0330

    Ready for merge

--- MainProject/MainWindow.xaml.cs
+++ MainProject/MainWindow.xaml.cs
@@ -26,24 +26,7 @@ namespace MainProject
     {
         public MainWindow()
         {
-            //User user = new User();
-            //user.Name = "Demo_Name";
-            //user.LastName = "Demo_LastName";
-            //user.Address = "This is a Address";
-            //user.Email_Unique = "Demo_Email@gmail.com";
-            //user.Gender = Gender.Female;
-            //user.Phone = "09123456789";
-            //user.UserName = "Demo_UserName";
-            //CustomerMainPage page = new CustomerMainPage(user);
-            //page.Show();
-
             InitializeComponent();
-            //LoginPage loginPage = new LoginPage();
-            //loginPage.Show();
-
-            AdminPage adminPage = new AdminPage(new Admin("UserName3","123123"));
-            adminPage.Show();
-            this.Close();
         }
     }
 }
