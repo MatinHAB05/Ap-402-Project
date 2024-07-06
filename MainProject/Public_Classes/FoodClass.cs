19e79a90ff5001f2ef8c66c0699d90cf326b4e11
Parham Mohammadi <parham200483@gmail.com>
2024-07-06 23:56:30 +0330

    Answer To Comment Added _ RestaurantPanel

--- MainProject/Public_Classes/FoodClass.cs
+++ MainProject/Public_Classes/FoodClass.cs
@@ -1,8 +1,11 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace MainProject.Public_Classes
 {
@@ -41,6 +44,17 @@ namespace MainProject.Public_Classes
             this.xBar = 0;
             this.RemNumber = RemNumber;
             this.comments_IN_ORDER = new List<FoodComment>();
+            string json = File.ReadAllText(@"C:\Users\ASUS\3D Objects\Project-Ap\SecondLayout\MainProject\Ap-402-Project\MainProject\JsonFiles\Restaurant\All_Restaurant.json");
+            List<Restaurant> restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(json);
+            int numberOfFoods = 0;
+            foreach (Restaurant re in restaurants)
+            {
+                foreach (Category category in re.Menu)
+                {
+                    numberOfFoods += category.Foods.Count();
+                }
+            }
+            FoodID = numberOfFoods + 1;
         }
 
 
