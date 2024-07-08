303becfe028bea688d51f93ff10381eadb1e8b10
Parham Mohammadi <parham200483@gmail.com>
2024-07-08 10:21:43 +0330

    the last commit

--- MainProject/MainWindow.xaml.cs
+++ MainProject/MainWindow.xaml.cs
@@ -32,60 +32,14 @@ namespace MainProject
         public MainWindow()
         {
             InitializeComponent();
-
-
             string directory = MainWindow.GetSourceFileDirectory();
-            MessageBox.Show(directory);
-            MessageBox.Show(Get_Dir_ALL_RESTAURANT_json());
-            //C:\User\ASUS\Desktop\Ap-402-Project\MainProject
-
-            //test
-            //List<Restaurant> test = JsonConvert.DeserializeObject<List<Restaurant>>(File.ReadAllText(Get_Dir_ALL_RESTAURANT_json()));
-            //List<User> testt= JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(Get_Dir_ALL_USERS_json()));
-            //List<ReceptionComment> sda = JsonConvert.DeserializeObject<List<ReceptionComment>>(File.ReadAllText(Get_Dir_ALL_RECEPTION_COMMENT_json()));
-            //List<Food_Point> asdadad = JsonConvert.DeserializeObject<List<Food_Point>>(File.ReadAllText(Get_Dir_ALL_FOOD_POINT_json()));
-            //List<Reception_Point> asdasd = JsonConvert.DeserializeObject<List<Reception_Point>>(File.ReadAllText(Get_Dir_ALL_RECEPTION_POINT_json()));
-            //List<FoodRequest> csd= JsonConvert.DeserializeObject<List<FoodRequest>>(File.ReadAllText(Get_Dir_ALL_FOOD_REQUEST_json()));
-            //List<Complaint> trg= JsonConvert.DeserializeObject<List<Complaint>>(File.ReadAllText(Get_Dir_ALL_COMPLAINTS_json()));
-            //List<Admin> xsdee = JsonConvert.DeserializeObject<List<Admin>>(File.ReadAllText(Get_Dir_ALL_ADMIN_json()));
-            //test
-
-
-
-
-            //matin start
-            //User user = new User();
-            //user.Name = "Demo_Name";
-            //user.LastName = "Demo_LastName";
-            //user.Address = "This is a Address";
-            //user.Email_Unique = "m9652973@gmail.com";
-            //user.Gender = Gender.Female;
-            //user.Phone = "09123456789";
-            //user.UserName = "Demo_UserName";
-            //CustomerMainPage page = new CustomerMainPage(user);
-            //page.Show();
-
             LoginPage loginPage = new LoginPage();
             loginPage.Show();
-
-            //138397
-            //string directory = MainWindow.GetSourceFileDirectory();
-            //directory = System.IO.Path.Combine(directory, @"LoginForm_Matin\BackMusic_Matin\Mix_BackMusic2.mp3");
-            //MusicWindowHIDE musicWindowHIDE = new MusicWindowHIDE(directory);
-            //musicWindowHIDE.Show();
-            //musicWindowHIDE.Hide();
+            directory = System.IO.Path.Combine(directory, @"LoginForm_Matin\BackMusic_Matin\Mix_BackMusic2.mp3");
+            MusicWindowHIDE musicWindowHIDE = new MusicWindowHIDE(directory);
+            musicWindowHIDE.Show();
+            musicWindowHIDE.Hide();
             this.Close();
-            //matin end
-
-
-
-            //parham start
-            string jsonString = File.ReadAllText(MainWindow.Get_Dir_ALL_RESTAURANT_json());
-            //List<Restaurant> restaurantsJsonData = System.Text.Json.JsonSerializer.Deserialize<List<Restaurant>>(jsonString);
-            //RestaurantPanel restaurantPanel = new RestaurantPanel(restaurantsJsonData[0]);
-            //this.Close();
-            //restaurantPanel.Show();
-            //parham end
         }
 
 
